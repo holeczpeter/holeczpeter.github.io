01b886d90d2960caa2ddb234a720978a90e2d4ce
holeczpeter <holecz.peter85@gmail.com>
2022-01-29 23:28:01 +0100

    fix button

--- js/script.js
+++ js/script.js
@@ -423,14 +423,15 @@ $(window).resize(function(){
 
 const cookieContainer = document.querySelector(".cookie-container");
 const cookieButton = document.querySelector(".cookie-btn");
-
-cookieButton.addEventListener("click", () => {
-  cookieContainer.classList.remove("active");
-  localStorage.setItem("cookieBannerDisplayed", "true");
-});
-
-setTimeout(() => {
-  if (!localStorage.getItem("cookieBannerDisplayed")) {
-    cookieContainer.classList.add("active");
-  }
-}, 2000);
+if(cookieButton){
+	cookieButton.addEventListener("click", () => {
+		cookieContainer.classList.remove("active");
+		localStorage.setItem("cookieBannerDisplayed", "true");
+	  });
+	  
+	  setTimeout(() => {
+		if (!localStorage.getItem("cookieBannerDisplayed")) {
+		  cookieContainer.classList.add("active");
+		}
+	  }, 2000);
+}
